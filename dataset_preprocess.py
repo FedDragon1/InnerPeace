ff0e589db197b2444130428f7de6ebdf47009f44
Fed_Dragon <fedelewu@outlook.com>
2023-09-09 13:24:21 -0700

    a

--- dataset_preprocess.py
+++ dataset_preprocess.py
@@ -1,29 +1,42 @@
-import os
-import pathlib
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
-from constants import *
 import tqdm
+
+from constants import *
 
 
 def image_to_csv(image_path):
     image = Image.open(image_path)
-    # image.mode == 'L"
+    # image.mode == 'L", size = (48, 48)
     arr = np.array(image)
     # plt.imshow(arr, cmap='gray')
     # plt.show()
     return ",".join(str(x) for x in arr.flatten())
 
 
+def get_heading():
+    return "emotion," + ",".join(str(i) for i in range(48 ** 2)) + '\n'
+
+
 def images_to_csv(folder):
     with open(DATASET_PATH / f"{folder}.csv", "w") as file:
+        file.write(get_heading())
         for emotion in tqdm.tqdm(list((DATASET_PATH / folder).iterdir()), desc=f"{folder}"):
             code = EMOTION_TO_INDEX[emotion.name]
             for image_path in tqdm.tqdm(list(emotion.iterdir()), desc=f"image"):
                 csv_line = image_to_csv(image_path)
                 csv_line = f"{code},{csv_line}\n"
                 file.write(csv_line)
+
+
+def dataset_size():
+    # test
+    test = sum(len(list(emotion.iterdir())) for emotion in (DATASET_PATH / "test").iterdir())
+    # train
+    train = sum(len(list(emotion.iterdir())) for emotion in (DATASET_PATH / "train").iterdir())
+
+    print(f"{test=}")
+    print(f"{train=}")
 
 
 def main():
@@ -33,3 +46,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # dataset_size()
